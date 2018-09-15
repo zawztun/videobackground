f08d8ae5d26b379f5b24ac6e7deef1a6aa759440
zawztun <33265355+zawztun@users.noreply.github.com>
2018-09-15 19:55:40 -0400

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#first").click(function(){
+        $("#1").toggle(2000);
+    });
+});
+
+$(document).ready(function(){
+    $("#second").click(function(){
+      $("#2").toggle(2000);  
+       
+    });
+});
+
+$(document).ready(function(){
+    $("#third").click(function(){
+        $("#3").toggle(2000);
+    });
+});
+
+
+
+
+
+/*window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+}*/
+
